2de52e7b89fac7e074aeb3aaab475358e216124f
Healwy <982973349@qq.com>
2020-12-18 00:41:51 +0800

    Initial commit

---
+++ entry/src/main/java/com/example/onelineedict/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.onelineedict;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
